d3c0d1149f69fed8b39bbb6fc40953bb1167beec
Thiago Araujo <thiago.araujotga@outlook.com>
2021-02-23 12:11:01 -0300

    Alterando controller verbo Post com tratamento de dados

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,10 +1,31 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController{
     async create(req: Request, resp: Response){
-        const body = req.body;
-        console.log(body);
-        return resp.send();
+        const { name, email } = req.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+
+        if(userAlreadyExists){
+            return response.status(400).json({
+                error: "User already exists!",
+            });
+        }
+
+        const user = usersRepository.create({
+            name,
+            email
+        });
+
+        await usersRepository.save(user);
+        
+        return resp.json(user);
     };
 }
 
